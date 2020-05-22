24b6cb1d78629062e66fcba2438a57bbe41cb445
BHAVYA NARANG <bhavya.narang2015@gmail.com>
2020-05-22 11:35:08 +0530

    check2

--- digit.py
+++ digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 from keras.preprocessing.image import ImageDataGenerator
@@ -15,34 +15,34 @@ from keras.utils import np_utils
 import keras
 
 
-# In[2]:
+# In[3]:
 
 
 (x_train, y_train),(x_test,y_test) =mnist.load_data()
 
 
-# In[3]:
+# In[4]:
 
 
 img_rows=x_train[0].shape[0]
 img_cols=x_train[0].shape[1]
 
 
-# In[4]:
+# In[5]:
 
 
 x_train = x_train.reshape(x_train.shape[0],img_rows, img_cols, 1)
 x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
 
 
-# In[5]:
+# In[6]:
 
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 
 
-# In[6]:
+# In[7]:
 
 
 
@@ -51,7 +51,7 @@ x_train /= 255
 x_test /= 255
 
 
-# In[7]:
+# In[8]:
 
 
 # Now we one hot encode outputs
@@ -59,40 +59,50 @@ y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
 
 
-# In[8]:
+# In[9]:
 
 
 num_classes = y_test.shape[1]
 num_pixels = x_train.shape[1] * x_train.shape[2]
 
 
-# In[9]:
+# In[10]:
 
 
 model=Sequential()
 
 
-# In[10]:
+# In[11]:
 
 
 input_shape = (img_rows, img_cols, 1)
 
 
-# In[11]:
+# In[12]:
 
 
-model.add(Conv2D(20, (5,5) , padding='same', input_shape=input_shape))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+def model_layers(model ,filter_qnt , size_filter , pool_size):
+    model.add(Conv2D(filter_qnt, (size_filter,size_filter) , padding='same', input_shape=input_shape))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(pool_size,pool_size), strides=(2,2)))
+    
+def add_dense_layer(neurons):   
+    model.add(Dense(neurons))
+    model.add(Activation("relu"))
 
-#model.add(Conv2D(50, (5,5) , padding='same'))
-#model.add(Activation("relu"))
-#model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+
+# In[13]:
+
+
+model_layers(model ,20,5,2 )
+#model_layers(model ,layer1)
+#model_layers(model ,layer3)
+
 
 #fullyconnected
 model.add(Flatten())
-model.add(Dense(500))
-model.add(Activation("relu"))
+add_dense_layer(neurons=500)
+
 
 #softmax for classifivcation
 model.add(Dense(num_classes))
@@ -103,7 +113,7 @@ model.compile(loss='categorical_crossentropy', optimizer = keras.optimizers.Adad
 print(model.summary())
 
 
-# In[ ]:
+# In[14]:
 
 
 # Training Parameters
@@ -118,18 +128,10 @@ history = model.fit(x_train, y_train,
 model.save("digit_lenet.h5")
 
 
-# In[ ]:
-
-
 scores = model.evaluate(x_test, y_test, verbose=1)
-
-
-# In[ ]:
-
 
 print('Test loss:', scores[0])
 print('Test accuracy:', scores[1])
-
 accuracy=scores[1]*100
 
 
