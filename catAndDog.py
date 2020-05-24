e561b775a31dc8e491ffa00051c060cb888bac9a
BHAVYA NARANG <bhavya.narang2015@gmail.com>
2020-05-24 16:18:29 +0530

    6 commit

--- catAndDog.py
+++ catAndDog.py
@@ -42,12 +42,12 @@ train_datagen = ImageDataGenerator(
         horizontal_flip=True)
 test_datagen = ImageDataGenerator(rescale=1./255)
 training_set = train_datagen.flow_from_directory(
-        '/training_set/',
+        '/task3/training_set',
         target_size=(64, 64),
         batch_size=32,
         class_mode='binary')
 test_set = test_datagen.flow_from_directory(
-        '/test_set/',
+        '/task3/test_set',
         target_size=(64, 64),
         batch_size=32,
         class_mode='binary')
